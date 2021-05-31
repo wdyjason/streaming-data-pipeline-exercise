b979dcc8315be1fae7f099c87f1de81a14ea2ab5
Yuan Zhang <zhangyuan@users.noreply.github.com>
2021-05-31 09:51:04 +0800

    create a gradle application

---
+++ src/test/java/com/example/HelloTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloTest {
+    @Test
+    void should_be_ok() {
+        assertTrue(true);
+    }
+}
