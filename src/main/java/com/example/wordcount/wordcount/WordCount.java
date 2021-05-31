61bd9333b55d489ba8638ef8ef23c422fa47f030
Yuan Zhang <zhangyuan@users.noreply.github.com>
2021-05-31 11:11:49 +0800

    create word count example

---
+++ src/main/java/com/example/wordcount/wordcount/WordCount.java
@@ -0,0 +1,19 @@
+package com.example.wordcount.wordcount;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class WordCount {
+//  Run `nc -lk 9999` from terminal before running main method.
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> dataStreamSource = env
+                .socketTextStream("localhost", 9999);
+        DataStream<Tuple2<String, Integer>> dataStream = new WordCountProcessor(dataStreamSource).perform();
+        dataStream.print();
+        env.execute("WordCount");
+    }
+
+}
---
+++ src/main/java/com/example/wordcount/wordcount/WordCountProcessor.java
@@ -0,0 +1,20 @@
+package com.example.wordcount.wordcount;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+
+public class WordCountProcessor {
+    private DataStreamSource<String> dataStreamSource;
+
+    public WordCountProcessor(DataStreamSource<String> dataStreamSource) {
+        this.dataStreamSource = dataStreamSource;
+    }
+
+    public DataStream<Tuple2<String, Integer>> perform() {
+        return dataStreamSource
+                .flatMap(new Splitter())
+                .keyBy(value -> value.f0)
+                .sum(1);
+    }
+}
