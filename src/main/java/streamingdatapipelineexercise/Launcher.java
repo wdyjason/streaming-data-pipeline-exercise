3108b895ee0ca3be48a1e1aae1725f3693ce3b41
Yuan Zhang <zhangyuan@users.noreply.github.com>
2021-06-10 11:16:03 +0800

    change mainClass to launcher

--- src/main/java/streamingdatapipelineexercise/Launcher.java
+++ src/main/java/streamingdatapipelineexercise/Launcher.java
@@ -1,10 +1,43 @@
 package streamingdatapipelineexercise;
 
-public class Launcher {
-    public static void main(String[] args) throws ClassNotFoundException {
-        String mainClass = args[0];
-        Class<?> aClass = Class.forName(mainClass);
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
-        System.out.println(aClass);
+public class Launcher {
+    private String mainClass;
+
+    public Launcher(String mainClass) {
+        this.mainClass = mainClass;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
+        if (args.length != 1) {
+            System.err.println("mainClass is not provided.");
+            System.exit(1);
+        }
+        new Launcher(args[0]).invoke();
+    }
+
+    private Constructor<?> findConstructor(Class<?> aClass) {
+        Constructor<?>[] declaredConstructors = aClass.getDeclaredConstructors();
+        return Arrays.stream(declaredConstructors).filter(x -> x.getParameters().length == 0).findFirst().get();
+    }
+
+    private Class<?> findMainClass(String mainClass) throws ClassNotFoundException {
+        return Class.forName(mainClass);
+    }
+
+    private void invoke() throws ClassNotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        Class<?> aClass = findMainClass(mainClass);
+
+        Constructor<?> constructor = findConstructor(aClass);
+
+        Object aInstance = constructor.newInstance();
+
+        Method executeMethod = aClass.getDeclaredMethod("execute");
+
+        executeMethod.invoke(aInstance);
     }
 }
